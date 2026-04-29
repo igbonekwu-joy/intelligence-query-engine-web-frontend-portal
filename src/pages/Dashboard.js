6210d9e6d8b7c2bf0eb53fdf0ff12f4519f63db0
Joy <joykenneth605@gmail.com>
2026-04-29 15:11:25 +0100

    feat: add csrf token implementation

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -35,7 +35,7 @@ export default function Dashboard() {
       setTotal(res.data.total);
     }
     catch (e) {
-      if(e.response.status === 400 || e.response.status === 401) {
+      if(e.response.status === 400 || e.response.status === 401 || e.response.status === 403) {
         navigate("/login");
       }
       else{
