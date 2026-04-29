ccd79640d98ae58c0a124c1f98db02260bac9041
Joy <joykenneth605@gmail.com>
2026-04-29 11:28:31 +0100

    feat(auth): add logout implementation

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,15 +1,31 @@
+import { Toaster } from "sonner";
 import api from "../api/axios";
+import Sidebar from "../components/Sidebar";
+import NavBar from "../components/NavBar";
 
 export default function Account() {
   const logout = async () => {
-    await api.post("/auth/logout");
-    window.location.href = "/login";
+    try {
+      await api.post("/auth/logout");
+      window.location.href = "/login";
+    }
+    catch(e) {
+      console.log(e);
+    }
   };
 
   return (
     <div>
-      <h2>Account</h2>
-      <button onClick={logout}>Logout</button>
+      <Toaster position="bottom-right" theme="dark" />
+      <Sidebar />
+      <div className="main-content">
+        <NavBar />
+
+        <div className="page-body" id="page-body">
+          <h2>Account</h2>
+          <button onClick={logout} className="btn-insighta btn-ghost-i">Logout</button>
+        </div>
+      </div>
     </div>
   );
 }
